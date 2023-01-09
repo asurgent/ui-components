7f693d94c0c56406e4cba0888af7e5c7c3111ec4
Kenneth Österholm <kenneth.osterholm@asurgent.se>
2023-01-09 13:52:32 +0100

    use toast to inform paste file from clipboard

--- src/Form/types/File/File.translation.js
+++ src/Form/types/File/File.translation.js
@@ -8,6 +8,7 @@ export default addTranslation({
     limitReached: 'Gränsen för antal bifogade filer har uppnåtts',
     fileToLarge: 'Filen får inte vara tyngre än 4 MB',
     uploadedTitle: 'Uppladdade files',
+    pastedFromClipboard: 'En fil lades till för uppladdning upp från urklippet',
   },
   en: {
     uploadedTitle: 'Uploaded files',
@@ -15,5 +16,6 @@ export default addTranslation({
     dropZonePlaceholder: '...or drag and drop a file here',
     limitReached: 'Maximum files has been reached',
     fileToLarge: 'The file must not be larger than 4 MB',
+    pastedFromClipboard: 'A fil was added for uppload from the clipboard',
   },
 });
